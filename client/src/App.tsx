3004bc675379b6cba99f534d0dec6e89c0ef2205
Jason <clone>
2024-03-12 04:24:43 +0800

    v2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,80 +1,141 @@
-import React, { ChangeEvent, useState, useEffect } from "react";
+import React, { useState, ChangeEvent } from "react";
 import axios from "axios";
 import {
   Button,
   Card,
   CardContent,
-  CardMedia,
   Typography,
+  TextField,
 } from "@mui/material";
 
 const App: React.FC = () => {
   const url = "http://localhost:5000/";
+  const [imageSrc, setImageSrc] = useState<string>(`${url}frame_feed`);
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [isCameraActive, setIsCameraActive] = useState<boolean>(true);
-  const [uploadedImageUrl, setUploadedImageUrl] = useState<string>("");
-
-  useEffect(() => {}, [uploadedImageUrl, isCameraActive]);
+  const [youtubeUrl, setYoutubeUrl] = useState<string>("");
+  const [descType, setDescType] = useState<string>("Live Camera");
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
+    if (event.target.files && event.target.files.length > 0) {
       setSelectedFile(event.target.files[0]);
     }
   };
 
-  const handleUpload = async () => {
-    if (selectedFile) {
-      const formData = new FormData();
-      formData.append("file", selectedFile);
-      try {
-        const response = await axios.post(`${url}upload`, formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        });
-        alert("File uploaded successfully");
-        const { filename } = response.data;
-        console.log("Uploaded file: ", filename);
-        setUploadedImageUrl(`${url}uploaded/${filename}`);
-        setIsCameraActive(false);
-      } catch (error) {
-        alert(error + " Error uploading file");
-      }
+  const handleYoutubeUrlChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setYoutubeUrl(event.target.value);
+  };
+
+  const handleImagePredict = async () => {
+    if (!selectedFile) {
+      alert("Please select a file first!");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("image", selectedFile);
+
+    try {
+      const response = await axios.post(`${url}image_predict`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        responseType: "blob", // handling the binary image response
+      });
+
+      // Create a URL for the blob response and update the image source
+      const imageUrl = URL.createObjectURL(response.data);
+      setImageSrc(imageUrl);
+      setIsCameraActive(false);
+      await switchToImage();
+    } catch (error) {
+      console.error("Error uploading image:", error);
     }
   };
 
   const switchToCamera = async () => {
     try {
       await axios.post(`${url}switch_to_camera`);
+      setImageSrc(`${url}frame_feed`);
       setIsCameraActive(true);
+      setDescType("Live Camera");
     } catch (error) {
       alert(error + " Error switching to camera");
     }
   };
 
+  const switchToYoutube = async () => {
+    try {
+      await axios.post(`${url}switch_to_youtube`, { youtubeUrl });
+      setImageSrc(`${url}youtube_feed`);
+      setIsCameraActive(false);
+      setDescType("Youbute Video");
+    } catch (error) {
+      alert("Error switching to youtube");
+    }
+  };
+
+  const switchToImage = async () => {
+    try {
+      await axios.post(`${url}switch_to_image`);
+      setIsCameraActive(false);
+      setDescType("Uploaded Image");
+    } catch (error) {
+      alert(error + " Error switching to image");
+    }
+  };
+
   return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
-      <Button variant="contained" color="primary" onClick={handleUpload}>
-        Upload
+    <div className="p-4">
+      {/* Add a text field for the image upload */}
+      <TextField
+        type="file"
+        inputProps={{ accept: "image/*" }}
+        onChange={handleFileChange}
+        style={{ display: "block", margin: "10px 0" }}
+      />
+      <Button variant="contained" color="primary" onClick={handleImagePredict}>
+        Upload and Predict
       </Button>
-      {!isCameraActive && ( // Conditionally render the switch to camera button
-        <Button variant="contained" color="secondary" onClick={switchToCamera}>
+
+      {/* Add a text field for the youtube URL */}
+      <TextField
+        type="url"
+        style={{ display: "block", margin: "10px 0" }}
+        value={youtubeUrl}
+        onChange={handleYoutubeUrlChange}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={switchToYoutube}
+        style={{ marginLeft: "10px" }}
+      >
+        Youtube predict
+      </Button>
+
+      {/* Add a button to switch to the camera feed */}
+      {!isCameraActive && (
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={switchToCamera}
+          style={{ marginLeft: "10px" }}
+        >
           Switch to Camera
         </Button>
       )}
 
-      <Card>
+      <Card sx={{ maxWidth: 1080, marginTop: "20px" }}>
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            {isCameraActive ? "Live Camera Feed" : "Uploaded Image"}
+            {descType}
           </Typography>
         </CardContent>
-        <CardMedia
-          component="img"
-          image={`${url}frame_feed`}
-          alt={isCameraActive ? "Live Camera Feed" : "Uploaded Image"}
-          sx={{ width: 900, height: 600 }}
+        <img
+          src={imageSrc}
+          alt={descType}
+          style={{ width: 1080, height: 720 }}
         />
       </Card>
     </div>
--- server/server.py
+++ server/server.py
@@ -1,45 +1,120 @@
-from flask import Flask, request, jsonify, Response, send_from_directory
+from flask import Flask, request, jsonify, Response, send_from_directory, send_file
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import cv2
 import os
 import sys
+import time
+
+from collections import deque
 from math import ceil
 from typing import Any
 import numpy as np
 from ultralytics import YOLO
-from enum import Enum
+import concurrent.futures as cf
+from vidgear.gears import CamGear
+from collections import Counter
 
-# from math import ceil
 
 sys.path.extend([".."])
+
 app = Flask(__name__)
 CORS(app)
 
-UPLOAD_FOLDER = "uploaded"
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
-ALLOWED_EXTENSIONS = {"mp4", "jpg", "jpeg", "png"}
+# UPLOAD_FOLDER = "uploaded"
+# app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+# ALLOWED_EXTENSIONS = {"mp4", "jpg", "jpeg", "png"}
+# os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
-current_source = "camera"  # 'camera' or 'file'
+# CURRENT_SOURCE = "camera"  # 'camera' or 'file'
+VIDEO_SOURCE = 0  # Default camera
+youtube = "http://www.youtube.com/watch?v=q0kPBRIPm6o"  # Default youtube live stream
+
+cap = cv2.VideoCapture(VIDEO_SOURCE)
+youtube_cap = CamGear(source=youtube, stream_mode=True, logging=True).start()
+
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+resize_width = 1280
+resize_height = 720
+if frame_width > 0:
+    resize_height = int((resize_width / frame_width) * frame_height)
+
 model = YOLO("../model/best_100.pt")
 
-os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
-Label = Enum(
-    "label",
-    [
-        "Hardhat",
-        "Mask",
-        "No Hardhat",
-        "No Mask",
-        "No Safety Vest",
-        "Person",
-        "Safety Cone",
-        "Safety Vest",
-        "Machinery",
-        "Vehicle",
-    ],
-)
+def predict_and_detect(model, img, conf=0.5) -> tuple:
+
+    # img = cv2.resize(img, (resize_width, resize_height))
+
+    img = cv2.resize(img, (resize_width, resize_height))
+
+    results = model.predict(img, conf=conf, verbose=False)
+    names = results[0].names
+    class_detections_values = []
+    for k, v in names.items():
+        class_detections_values.append(results[0].boxes.cls.tolist().count(k))
+    # create dictionary of objects detected per class
+    classes_detected = dict(zip(names.values(), class_detections_values))
+
+    if not results:
+        results = []
+
+    for result in results:
+        for box in result.boxes:
+            x1 = int(box.xyxy[0][0])
+            y1 = int(box.xyxy[0][1])
+            x2 = int(box.xyxy[0][2])
+            y2 = int(box.xyxy[0][3])
+            label = result.names[int(box.cls[0])]
+            conf = ceil((box.conf[0] * 100))
+
+            text_color = (255, 255, 255)
+            box_color = get_box_color(int(box.cls[0]))
+
+            # Draw the rectangle background for text
+            text = f"{label} {conf:.2f}"
+            (text_width, text_height), _ = cv2.getTextSize(
+                text, cv2.FONT_HERSHEY_SIMPLEX, 0.75, 2
+            )
+            cv2.rectangle(
+                img,
+                (x1, y1 - text_height - 15),
+                (x1 + text_width, y1),
+                box_color,
+                -1,
+            )
+
+            # Draw the text on top of the rectangle
+            cv2.putText(
+                img,
+                text,
+                (x1, y1 - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.75,
+                text_color,
+                2,
+            )
+            cv2.rectangle(img, (10, 60), (10 + 300, 20), (0, 0, 0), -1)
+
+            cv2.putText(
+                img,
+                f"Total Person: {classes_detected['Person']}",
+                (20, 50),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (255, 255, 255),
+                2,
+            )
+
+            # Draw the bounding box
+            cv2.rectangle(img, (x1, y1), (x2, y2), box_color, 2)
+    return img, results
+
+
+def process_frame(frame) -> np.ndarray:
+    result_frame, _ = predict_and_detect(model, frame)
+    return result_frame
 
 
 # BRG color
@@ -61,115 +136,122 @@ def get_box_color(index: int) -> tuple:
             return (255, 0, 0)  # Blue
 
 
-def allowed_file(filename) -> bool:
-    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+# def allowed_file(filename) -> bool:
+#     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route("/upload", methods=["POST"])
-def upload_file() -> object:
-    if "file" not in request.files:
-        return jsonify({"error": "No file part"}), 400
-    file = request.files["file"]
-    if file.filename == "":
-        return jsonify({"error": "No selected file"}), 400
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-        file.save(filepath)
-        global current_source
-        current_source = filepath
-        return (
-            jsonify({"message": "File uploaded successfully", "filename": filename}),
-            200,
-        )
-    else:
-        return jsonify({"error": "File type not allowed"}), 400
+@app.route("/image_predict", methods=["POST"])
+def predict_image() -> object:
+    if "image" not in request.files:
+        return jsonify({"error": "No image part"}), 400
+
+    file = request.files["image"]
+    if file:
+        # Read the image file in a NumPy array format
+        npimg = np.fromfile(file, np.uint8)
+        img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+        # Process the image directly with the model
+        result_frame = process_frame(img)
+
+        # Convert the image with predictions back to bytes and send as a response
+        _, buffer = cv2.imencode(".jpg", result_frame)
+        return Response(buffer.tobytes(), mimetype="image/jpeg")
+
+    return jsonify({"error": "Invalid file type"}), 400
 
 
 @app.route("/frame_feed")
 def frame_feed() -> Response:
     def generate() -> Any:
-        global current_source
-        cap = cv2.VideoCapture(0)
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-        if current_source == "camera":
+
+        skip_frames = 2  # Number of frames to skip before processing the next one
+        frame_count = 0
+        # if CURRENT_SOURCE == "camera":
+        with cf.ThreadPoolExecutor() as executor:
             while True:
                 ret, frame = cap.read()
                 if not ret:
-                    continue
+                    break
+                frame_count = 1 + frame_count
+                if frame_count % skip_frames != 0:
+                    continue  # Skip this frame
 
-                if not (result := model.predict(frame, verbose=False)):
-                    return None
+                future = executor.submit(process_frame, frame)
+                result_frame = future.result()
 
-                for detection in result[0].boxes:
-                    if ceil((detection.conf[0] * 100)) > 0.5 * 100:
-                        x1 = int(detection.xyxy[0][0])
-                        y1 = int(detection.xyxy[0][1])
-                        x2 = int(detection.xyxy[0][2])
-                        y2 = int(detection.xyxy[0][3])
-                        label = Label(int(detection.cls[0]) + 1).name
-                        conf = ceil((detection.conf[0] * 100))
-
-                        # background_color = (255, 69, 0)
-                        text_color = (255, 255, 255)
-                        box_color = get_box_color(int(detection.cls[0]))
-
-                        # Draw the rectangle background for text
-                        text = f"{label} {conf:.2f}"
-                        (text_width, text_height), _ = cv2.getTextSize(
-                            text, cv2.FONT_HERSHEY_SIMPLEX, 0.75, 2
-                        )
-                        cv2.rectangle(
-                            frame,
-                            (x1, y1 - text_height - 15),
-                            (x1 + text_width, y1),
-                            box_color,
-                            -1,
-                        )
-
-                        # Draw the text on top of the rectangle
-                        cv2.putText(
-                            frame,
-                            text,
-                            (x1, y1 - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX,
-                            0.75,
-                            text_color,
-                            2,
-                        )
-
-                        # Draw the bounding box
-                        cv2.rectangle(frame, (x1, y1), (x2, y2), box_color, 2)
-
-                ret, buffer = cv2.imencode(".jpg", frame)
-                if not ret:
-                    print("Failed to encode image")
-                    continue
-                frame = buffer.tobytes()
+                ret, buffer = cv2.imencode(".jpg", result_frame)
+                encoded_frame = buffer.tobytes()
 
                 yield (
-                    b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+                    b"--frame\r\n"
+                    b"Content-Type: image/jpeg\r\n\r\n" + encoded_frame + b"\r\n"
                 )
-        else:
-            frame = cv2.imread(current_source)
-            if frame is None:
-                print("Failed to read image from file")
-                return
+                time.sleep(0.01)
+
+    return Response(generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+@app.route("/youtube_feed")
+def youtube_feed() -> Response:
+    def generate() -> Any:
+
+        skip_frames = 2
+        frame_count = 0
+
+        with cf.ThreadPoolExecutor() as executor:
+            while True:
+                frame = youtube_cap.read()
+                if frame is None:
+                    break
+
+                frame_count = 1 + frame_count
+                if frame_count % skip_frames != 0:
+                    continue  # Skip this frame
+
+                future = executor.submit(process_frame, frame)
+                result_frame = future.result()
+
+                ret, buffer = cv2.imencode(".jpg", result_frame)
+                encoded_frame = buffer.tobytes()
+
+                yield (
+                    b"--frame\r\n"
+                    b"Content-Type: image/jpeg\r\n\r\n" + encoded_frame + b"\r\n"
+                )
+                time.sleep(0.01)
 
     return Response(generate(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 @app.route("/switch_to_camera", methods=["POST"])
-def switch_to_camera() -> object:
-    global current_source
-    current_source = "camera"
+def switch_to_camera() -> Response:
+    global CURRENT_SOURCE
+    # global cap
+    # cap = cv2.VideoCapture(VIDEO_SOURCE)
+    CURRENT_SOURCE = "camera"
     return jsonify({"message": "Switched to camera"}), 200
 
 
-@app.route("/upload/<filename>")
-def uploaded_file(filename) -> str:
-    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+@app.route("/switch_to_youtube", methods=["POST"])
+def switch_to_youtube() -> Response:
+    global CURRENT_SOURCE
+    global youtube_cap
+    global youtube
+    CURRENT_SOURCE = "youtube"
+    youtube_url = request.json["youtubeUrl"]
+    if youtube_url == "":
+        youtube_url = "http://www.youtube.com/watch?v=q0kPBRIPm6o"
+
+    youtube_cap = CamGear(source=youtube_url, stream_mode=True, logging=False).start()
+    return jsonify({"message": "Switched to youtube"}), 200
+
+
+@app.route("/switch_to_image", methods=["POST"])
+def switch_to_image() -> Response:
+    global CURRENT_SOURCE
+    CURRENT_SOURCE = "image"
+    return jsonify({"message": "Switched to image"}), 200
 
 
 if __name__ == "__main__":
